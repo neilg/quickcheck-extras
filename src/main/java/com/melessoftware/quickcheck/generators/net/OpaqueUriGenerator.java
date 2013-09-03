299e1767e2b19e2efe6ed3858b1ec59c90c9be83
Neil Green <neil@melessoftware.com>
2013-09-03 20:41:56 +0100

    uri scheme generator and slightly bodged opaque uri generator

--- src/main/java/com/melessoftware/quickcheck/generators/net/OpaqueUriGenerator.java
+++ src/main/java/com/melessoftware/quickcheck/generators/net/OpaqueUriGenerator.java
@@ -1,31 +1,32 @@
 package com.melessoftware.quickcheck.generators.net;
 
-import static net.java.quickcheck.generator.PrimitiveGenerators.characters;
-import static net.java.quickcheck.generator.PrimitiveGenerators.strings;
+import static com.melessoftware.quickcheck.generators.net.UriSchemeGenerator.schemes;
+import static net.java.quickcheck.generator.PrimitiveGenerators.letterStrings;
 
 import java.net.URI;
 
 import net.java.quickcheck.Generator;
+import net.java.quickcheck.generator.support.StringGenerator;
 
 public class OpaqueUriGenerator implements Generator<URI> {
 
     private final Generator<String> schemeGenerator;
+    private final Generator<String> specificPartGenerator;
 
     public OpaqueUriGenerator() {
-        this(strings('a', 'z')
-                .add(characters('A', 'Z'))
-                .add(characters('0', '9'))
-                .add(characters("+-.")));
+        this(schemes(), letterStrings(1, StringGenerator.MAX_LENGTH));
     }
 
-    public OpaqueUriGenerator(Generator<String> schemeGenerator) {
+    public OpaqueUriGenerator(Generator<String> schemeGenerator, Generator<String> specificPartGenerator) {
         this.schemeGenerator = schemeGenerator;
+        this.specificPartGenerator = specificPartGenerator;
     }
 
 
     @Override
     public URI next() {
         String scheme = schemeGenerator.next();
-        return URI.create(scheme+":");
+        String specificPart = specificPartGenerator.next();
+        return URI.create(scheme + ":" + specificPart);
     }
 }
--- src/main/java/com/melessoftware/quickcheck/generators/net/UriSchemeGenerator.java
+++ src/main/java/com/melessoftware/quickcheck/generators/net/UriSchemeGenerator.java
@@ -7,16 +7,22 @@ import net.java.quickcheck.Generator;
 
 public class UriSchemeGenerator implements Generator<String> {
 
-    private final Generator<String> firstChar = strings(1, 1)
+    private final Generator<String> headCharGenerator = strings("A", 1, 1)
             .add(characters('a', 'z'))
-            .add(characters('A', 'Z'));
+            .add(characters('B', 'Z'));
 
-    private final Generator<String> rest = strings('a', 'z')
+    private final Generator<String> tailStringGenerator = strings('a', 'z')
             .add(characters('A', 'Z'))
             .add(characters("+-."));
 
     @Override
     public String next() {
-        return firstChar.next() + rest.next();
+        final String head = headCharGenerator.next();
+        final String tail = tailStringGenerator.next();
+        return head + tail;
+    }
+
+    public static Generator<String> schemes() {
+        return new UriSchemeGenerator();
     }
 }
